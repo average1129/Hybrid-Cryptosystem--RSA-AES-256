e1b7345c93990db802ef991bd1b9bd7b2f5ccdff
average1129 <71325016+average1129@users.noreply.github.com>
2021-07-26 02:37:19 +0530

    Add files via upload

---
+++ Build_Env.sh
@@ -0,0 +1,17 @@
+#Building essential packages 
+
+sudo apt-get update
+sudo apt-get install build-essential gcc make cmake cmake-gui cmake-curses-gui
+sudo apt-get install -y doxygen mercurial
+sudo apt-get install m4
+sudo apt-get install valgrind
+
+#building gmp ibrary 
+hg clone https://gmplib.org/repo/gmp-6.2/ gmp
+sudo apt-get install autoconf libtool
+cd gmp/
+./.bootstrap
+./.configure --host=x86_64-linux-gnu --disable-static --enable-shared
+sudo make -j8
+sudo make check
+sudo make install
